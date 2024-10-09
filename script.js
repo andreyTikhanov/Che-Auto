1e972b14d2f019436bddb96880e96146d256cbcf
Andrey Tikhanov <andrei.tihanov@mail.ru>
2024-10-10 01:29:04 +0400

    Изменение слогана

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbarCollapse = document.querySelector('.navbar-collapse');
 
     navbarToggler.addEventListener('click', function() {
-        navbarCollapse.classList.toggle('show'); // Используйте класс 'show' для управления видимостью
+        navbarCollapse.classList.toggle('show'); 
     });
     document.addEventListener('click', function(event) {
         if (!navbarCollapse.contains(event.target) && !navbarToggler.contains(event.target)) {
-            navbarCollapse.classList.remove('show'); // Удалите класс 'show', если кликнули вне меню
+            navbarCollapse.classList.remove('show'); 
         }
     });
 });
